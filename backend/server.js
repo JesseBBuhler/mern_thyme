62c67003bcf023e575132834f60901d3fd4fd8e7
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-03-14 15:50:55 -0600

    create basic api routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
+const userRoutes = require("./routes/user");
+const blogRoutes = require("./routes/blog");
+const recipeRoutes = require("./routes/recipe");
 
 const express = require("express");
 const app = express();
 
-const sayHi = (req, res) => {
-  res.status(200).json({ mssg: "Hello World" });
-};
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-app.get("/hi", sayHi);
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/recipe", recipeRoutes);
 
+//start app
 app.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
 });
