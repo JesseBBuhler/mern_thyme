8ccf97fcc6855a2bd08b39cf548524b8852d5016
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-03-16 10:52:23 -0600

    add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,97 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    accessLevel: {
+      type: String,
+      required: true,
+    },
+    standing: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// static signup method
+// cannot use arrow function in functions that use "this"
+userSchema.statics.signup = async function (userName, email, password) {
+  //validation
+  if (!email || !password || !username) {
+    throw Error("All fields must be filled");
+  }
+
+  const emailExists = await this.findOne({ email });
+  const userNameExists = await this.findOne({ userName });
+
+  if (emailExists) {
+    throw Error("Email is already in use");
+  }
+
+  if (userNameExists) {
+    throw Error("Username is already in use");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    userName,
+    email,
+    password: hash,
+    accessLevel: "user",
+    standing: "default",
+  });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (userName, password) {
+  if (!userName || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ userName });
+
+  if (!user) {
+    throw Error("Incorrect User Name");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
