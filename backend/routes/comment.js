c9783b0048ae2e98d5eacca47f81917dc56ccc12
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-03-16 09:05:45 -0600

    merge route params for comment routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.get("/", (req, res) => {
   res.send("get info for all comments on blog post with id " + req.params.id);
