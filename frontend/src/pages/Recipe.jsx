8f750ae39048b679d67882c667fd22096066f794
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-10-30 18:31:26 -0600

    put recipes in recipe page

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRecipesContext } from "../hooks/useRecipeContext";
+
+//components
+import RecipeCard from "../components/RecipeCard";
 
 function Recipe() {
-  return <div>This will be the recipe page</div>;
+  const { recipes, dispatch } = useRecipesContext();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch("/api/public/recipe");
+        if (response.ok) {
+          const json = await response.json();
+          dispatch({ type: "SET_RECIPES", payload: json });
+        } else {
+          console.error("Request failed");
+        }
+      } catch (error) {
+        console.error("Fetch error", error);
+      }
+    };
+    fetchRecipes();
+  }, [dispatch]);
+
+  return (
+    <section className="recipe-results">
+      <div className="recipe-list">
+        {recipes &&
+          recipes.map((recipe) => (
+            <RecipeCard key={recipe._id} recipe={recipe}></RecipeCard>
+          ))}
+      </div>
+    </section>
+  );
 }
 
 export default Recipe;
