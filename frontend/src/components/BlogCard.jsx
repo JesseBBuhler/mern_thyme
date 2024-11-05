267584b096b9f8a0eacfe20549dff709ec0ec538
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-11-05 16:32:46 -0700

    rearrange blog card

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,18 +1,20 @@
 function BlogCard({ blog }) {
   return (
     <div className="blog-card">
-      <h4>{blog.title}</h4>
       <img
         className="blog-cover-img"
         src={`${process.env.PUBLIC_URL}/blogimg/${blog.coverImgURL}`}
         alt={blog.imgAlt}
       ></img>
-      <div className="tags">
-        <ul>
-          {blog.tags.map((tag, index) => {
-            return <li key={index}>{tag}</li>;
-          })}
-        </ul>
+      <div className="blog-summary">
+        <h4>{blog.title}</h4>
+        <div className="tags">
+          <ul>
+            {blog.tags.map((tag, index) => {
+              return <li key={index}>{tag}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
